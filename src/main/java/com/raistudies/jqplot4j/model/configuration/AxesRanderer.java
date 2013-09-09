ca8fdc008734c6d25bcf3b6922233d42868fe45d
Rahul <Rahul@10.1.7.140>
2013-09-09 17:30:11 +0530

    - Adding Axes Randerers

--- src/main/java/com/raistudies/jqplot4j/model/configuration/AxesRanderer.java
+++ src/main/java/com/raistudies/jqplot4j/model/configuration/AxesRanderer.java
@@ -5,5 +5,21 @@ package com.raistudies.jqplot4j.model.configuration;
  * @author Rahul
  */
 public enum AxesRanderer {
+    LinearAxisRenderer(""),
+    DateAxisRenderer("$.jqplot.DateAxisRenderer"),
+    CanvasAxisLabelRenderer("$.jqplot.CanvasAxisLabelRenderer"),
+    CategoryAxisRenderer("$.jqplot.CategoryAxisRenderer"),
+    CanvasAxisTickRenderer("$.jqplot.CanvasAxisTickRenderer"),
+    LogAxisRenderer("$.jqplot.LogAxisRenderer"),
+    MekkoAxisRenderer("$.jqplot.MekkoAxisRenderer");
     
+    private String jsRanderer;
+
+    private AxesRanderer(String jsRanderer) {
+        this.jsRanderer = jsRanderer;
+    }
+
+    public String getJsRanderer() {
+        return jsRanderer;
+    }
 }
